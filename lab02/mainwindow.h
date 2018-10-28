6cc08c0b637647d0d96f7d945f1e6fa9599b08e8
Carriecl <18795720920@163.com>
2018-10-29 00:08:22 +0800

    根据实验指导书，分析代码是如何实现窗口界面设计的，再结合Qt文档和原有代码，完成实验二要求的绘制文本框、菱形和图片的任务。

---
+++ lab02/mainwindow.h
@@ -0,0 +1,41 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include "centerframe.h"
+#include <QMainWindow>
+
+class QLabel;
+class QComboBox;
+class QSpinBox;
+class QToolButton;
+//class DrawWidget;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = 0);
+
+    ~MainWindow();
+
+    void createToolBar();
+
+public slots:
+    void penStyleChangged(int index=0);
+
+    void penColorChangged();
+
+private:
+    CenterFrame *centerFrame;       //用户界面中心窗口
+    QLabel *styleLabel;
+    QComboBox *styleComboBox;       //画笔风格下拉框
+    QLabel *widthLabel;
+    QSpinBox *widthSpinBox;         //画笔线宽spinbox
+    QToolButton *colorBtn;          //颜色选择工具栏按键
+    QToolButton *clearBtn;          //“清除”工具栏按键
+};
+#endif // MAINWINDOW_H
+
+
+
+
